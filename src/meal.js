9ed29d126bee7ec569730d439809a0093a4360e7
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-02-07 13:13:06 -0700

    initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,72 @@
-function nameMenuItem() {
+
+function nameMenuItem(name) {
+    var deliciousMenu = `Delicious ${name}`;
+
+
+    return deliciousMenu;
+}
+
+nameMenuItem("Pizza");
+nameMenuItem("Burrito");
+nameMenuItem("Taco");
+
+function createMenuItem(name, price, type) {
+    var menuItem = {
+      name: name,
+      price: price,
+      type: type,
+    }
+
+    return menuItem;
+}
+
+createMenuItem("Pizza", 10.99, "dinner");
+
+
+function addIngredients(item, ingredients) {
+
+ if (ingredients.includes(item)) {
+   return ingredients
+ } else {
+   ingredients.push(item);
+ }
+  return ingredients;
+}
+
+
+
+function formatPrice(price) {
+  var formattedPrice = "$" + price;
+
+  return formattedPrice;
+}
+
+function decreasePrice(price){
+  var decreasedPrice = price * .9;
+
+  return decreasedPrice;
 
 }
+
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+
+  return recipe;
+
+}
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,7 +1,45 @@
+function takeOrder(order, deliveryOrders) {
+    if (deliveryOrders.length < 3) {
+      deliveryOrders = deliveryOrders.push(order);
+    }
+
+
+}
+
+function refundOrder(order, deliveryOrders) {
+    if (order === 1) {
+      deliveryOrders.shift(order);
+    } else {
+      deliveryOrders.splice(1 , 1);
+    }
+
+}
+
+function listItems(listOfOrders) {
+   var list = "";
+   for (var i = 0; i < listOfOrders.length; i++) {
+     list = list + listOfOrders[i].item + ", ";
+   }  return list.slice(0, -2)
+
+
+}
+
+
+
+function searchOrder(deliveryOrders, orderItem) {
+    if (deliveryOrders[0].item === orderItem ||
+        deliveryOrders[1].item === orderItem) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
